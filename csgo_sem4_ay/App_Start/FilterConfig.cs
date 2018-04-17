0861e3a0ffed82b32788f4e6210a2a24c6e5c152
Hunle <32506812+Hunle@users.noreply.github.com>
2018-04-17 20:56:22 +0200

    Add project files.

---
+++ csgo_sem4_ay/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace csgo_sem4_ay
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ csgo_sem4_ay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(csgo_sem4_ay.Startup))]
+namespace csgo_sem4_ay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
