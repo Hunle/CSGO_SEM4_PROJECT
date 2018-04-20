0555529ecb02e458ff69170a2dcefec723a023cd
Arnes Kovacevic <1061725@ucn.dk>
2018-04-20 11:52:09 +0200

    GetConnectionString

---
+++ DB/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class DataAccess
+    {
+        private static DataAccess instance = null; 
+
+
+        private DataAccess()
+        {
+
+        }
+        public static DataAccess Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataAccess();
+                }
+                return instance;
+            }
+        }
+        public string GetConnectionString()
+        {
+            return "Server=kraka.ucn.dk;Database=dmaa0916_201960;User Id=dmaa0916_201960;Password=Password1!;Integrated Security=false;MultipleActiveResultSets=true";
+        }
+
+    }
+}
