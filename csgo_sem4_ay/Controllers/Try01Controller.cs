da6adf85ac44d171c68de454e0a38840a6b19e2b
Arnes Kovacevic <1061725@ucn.dk>
2018-04-24 12:34:43 +0200

    1

---
+++ csgo_sem4_ay/Controllers/Try01Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace csgo_sem4_ay.Controllers
+{
+    public class Try01Controller : Controller
+    {
+        // GET: Try01
+        public String Index()
+        {
+            return "Test 001";
+        }
+    }
+}
