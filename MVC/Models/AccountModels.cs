60e6b8c46e49158ef70a814aea85f0bdd81220ee
Arnes Kovacevic <1061725@ucn.dk>
2018-04-24 13:09:00 +0200

    Testing MVC

---
+++ MVC/Models/AccountModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using Models;
+using DB;
+
+
+namespace MVC.Models
+{
+    public class AccountModels
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class DataAccess : DbContext
+    {
+        DbSet<AccountModels> Accounts { get; set; }
+    }   
+}
+
+    
