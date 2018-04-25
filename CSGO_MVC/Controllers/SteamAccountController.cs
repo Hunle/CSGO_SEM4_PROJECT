108648a301234bdf435002086759d453c28c42e6
Arnes Kovacevic <1061725@ucn.dk>
2018-04-25 11:41:47 +0200

    ezfix

---
+++ CSGO_MVC/Controllers/SteamAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSGO_MVC.Controllers
+{
+    public class SteamAccountController : Controller
+    {
+        // GET: SteamAccount
+        public String Index()
+        {
+            return "Test Works";
+        }
+    }
+}
---
+++ CSGO_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSGO_MVC.MvcApplication" Language="C#" %>
