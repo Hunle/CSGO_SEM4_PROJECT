90d03928978f713ef1edb23ca566f89f3e496a46
Hunle <32506812+Hunle@users.noreply.github.com>
2018-05-14 23:44:06 +0200

    roulette bugger stadig (vi skal bruge jquery tho)

---
+++ CSGO_MVC/Content/MyScripts/jQueryRoulette.js
@@ -0,0 +1,7 @@
+﻿$('.RouletteWheel').click(function () {
+    $(this).css({
+        "-webkit-transform": "rotate(90deg)",
+        "-moz-transform": "rotate(90deg)",
+        "transform": "rotate(740deg)" /* For modern browsers(CSS3)  */
+    });
+});
