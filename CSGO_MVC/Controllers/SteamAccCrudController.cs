23c1476c4ce71bed054ebc8874b2abe9e1d87e34
Farzad\Faz <fazzerking1@gmail.com>
2018-05-16 22:03:20 +0200

    Revert "Kan ikke teste om login virker, register virker har tilføjet alt jeg har lavet."
    This reverts commit 29ecf5455a67b739aa3d24a7b2b0c4f919145608.

--- CSGO_MVC/Controllers/SteamAccCrudController.cs
+++ CSGO_MVC/Controllers/SteamAccCrudController.cs
@@ -5,42 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CSGO_MVC.Arnes_Repo;
-using System.Web.Security;
 
 namespace CSGO_MVC.Controllers
 {
-    [Authorize]
     public class SteamAccCrudController : Controller
     {
         private IRepository<SteamAccount> AccountRepo = null;
         private BalanceController bctrl = new BalanceController();
-        AccountApplication userApp = new AccountApplication();
-        SessionContext context = new SessionContext();
 
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(SteamAccount account)
-        {
-            var authenticatedUser = userApp.GetByUsernameAndPassword(account);
-            if (authenticatedUser != null)
-            {
-                context.SetAuthenticationToken(authenticatedUser.Id.ToString(), false, authenticatedUser);
-                return RedirectToAction("Index", "Home");
-            }
-
-            return View();
-        }
-
-        public ActionResult Logout()
-        {
-            FormsAuthentication.SignOut();
-            return RedirectToAction("Index", "Home");
-        }
 
         public SteamAccCrudController()
         {
