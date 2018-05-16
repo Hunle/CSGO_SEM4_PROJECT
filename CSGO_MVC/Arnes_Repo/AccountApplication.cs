5d4a186a95deac15c2c628d5519767c5ee01c7ce
Farzad\Faz <fazzerking1@gmail.com>
2018-05-16 22:13:28 +0200

    Lavet register, knappen er på forsiden i højre hjørne også som "Register" har ikke testet om login virker.

---
+++ CSGO_MVC/Arnes_Repo/AccountApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSGO_MVC.Models;
+using DB.Arnes_Repo;
+
+namespace CSGO_MVC.Arnes_Repo
+{
+    public class AccountApplication
+    {
+        
+            UserRepository userRepo = new UserRepository();
+            public SteamAccount GetByUsernameAndPassword(SteamAccount steam)
+            {
+                return userRepo.GetByUsernameAndPassword(steam);
+            }
+        }
+    
+}
